2c49aee853af0eb557f7d1c2d3163413c5b8712c
Rob Richardson <robrich@robrich.org>
2015-11-06 00:32:39 -0800

    Ensure we begin OAuth from the correct domain

--- src/Web/Controllers/LoginController.cs
+++ src/Web/Controllers/LoginController.cs
@@ -1,16 +1,29 @@
 ﻿namespace ApiInator.Web.Controllers {
+    using System;
     using System.Threading.Tasks;
     using Microsoft.AspNet.Authentication.Cookies;
+    using Microsoft.AspNet.Hosting;
     using Microsoft.AspNet.Http.Authentication;
     using Microsoft.AspNet.Mvc;
 
     public class LoginController : Controller {
+        private readonly IHostingEnvironment env;
+
+        public LoginController(IHostingEnvironment Env) {
+            if (Env == null) {
+                throw new ArgumentNullException(nameof(Env));
+            }
+            this.env = Env;
+        }
 
         public AuthenticationManager Authentication {
             get { return this.HttpContext.Authentication; }
         }
 
         public IActionResult Index() {
+            if (this.Request.Host.ToString() != "www.api-inator.com" && !this.env.IsDevelopment()) {
+                return this.Redirect("http://www.api-inator.com/login"); // the url must leave from the domain it should return to
+            }
             return new ChallengeResult("GitHub", new AuthenticationProperties {RedirectUri = "/"});
         }
         
